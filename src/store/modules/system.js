43467ff0a4d2a37b1bb7f461bf430224a371f727
lihui <81438234@qq.com>
2021-05-08 18:05:22 +0800

    1.添加eslint校验 2.代码格式化 3.把vuex拆分，添加vuser、system模块 4.添加vue.config.js配置文件 5.登录、登出使用vuex模式 6.对用户登录账号使用mock模拟进行账号密码错误校验

---
+++ src/store/modules/system.js
@@ -0,0 +1,69 @@
+/**
+ * 状态管理--系统配置（刷新页面会消失，要兼容浏览器需要本地存一份）
+ * @description 系统配置相关的数据全部存于该文件
+ * @Author lss 
+ * @Date 2021-3-8
+ */
+
+const state = {
+	tagsList: [],	// 标签页数组
+	collapse: true,	// 菜单折叠
+}
+
+const mutations = {
+	delTagsItem(state, data) {
+		state
+	        .tagsList
+	        .splice(data.index, 1);
+	},
+	setTagsItem(state, data) {
+	    state
+	        .tagsList
+	        .push(data)
+	},
+	clearTags(state) {
+	    state.tagsList = []
+	},
+	closeTagsOther(state, data) {
+	    state.tagsList = data;
+	},
+	closeCurrentTag(state, data) {
+	    for (let i = 0, len = state.tagsList.length; i < len; i++) {
+	        const item = state.tagsList[i];
+	        if (item.path === data.$route.fullPath) {
+	            if (i < len - 1) {
+	                data
+	                    .$router
+	                    .push(state.tagsList[i + 1].path);
+	            } else if (i > 0) {
+	                data
+	                    .$router
+	                    .push(state.tagsList[i - 1].path);
+	            } else {
+	                data
+	                    .$router
+	                    .push('/');
+	            }
+	            state
+	                .tagsList
+	                .splice(i, 1);
+	            break;
+	        }
+	    }
+	},
+	// 侧边栏折叠
+	hadndleCollapse(state, data) {
+	    state.collapse = data;
+	}
+}
+
+const actions = {
+	
+}
+
+export default {
+	namespaced: true,
+	state,
+	mutations,
+	actions
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,123 @@
+/**
+ * 状态管理--用户（刷新页面会消失，要兼容浏览器需要本地存一份）
+ * @description 用户相关的数据全部存于该文件
+ * @Author lss 
+ * @Date 2021-3-8
+ */
+// import cache from '@/common/js/cache'
+import {
+	login,
+	logout,
+	getUserInfo
+} from '@/api/user'
+
+const state = {
+	token: '',
+	openid: null,
+	userInfo: {},
+	wxInfo: {}, 	// 微信授权后的临时信息
+}
+
+// 更改 Vuex 的 store 中的状态的唯一方法是提交 mutation。统一入口，常量命名，必须是同步函数
+// 使用方法：可以在组件中使用 this.$store.commit('user/xxx') 提交 mutation。但是建议少直接调用，而是通过Action 方法触发
+const mutations = {
+	DO_LOGIN(state, payload) {
+		// state.loginProvider = payload.provider;
+		state.token = payload.userInfo.token;
+		// 缓存本地，兼容H5刷新。默认8小时候过期
+		/* cache.put('token', payload.userInfo.token, 60 * 60 * 8)
+		cache.put('userInfo', payload.userInfo, 60 * 60 * 8) 	// 只包含部分用户信息 */
+		sessionStorage['token'] = payload.userInfo.token
+		sessionStorage['userInfo'] = JSON.stringify(payload.userInfo) 
+	},
+	DO_LOGOUT(state) {
+		state.token = ''
+		state.openid = null
+		// 登出后清除相关状态数据 2021/3/9
+		sessionStorage.clear()
+	},
+	SET_OPENID(state, openid) {
+		state.openid = openid
+	},
+	// 设置用户信息
+	SET_USERINFO(state, payload) {
+		state.userInfo = payload.userInfo
+		// cache.put('userInfo', payload.userInfo, 60 * 60 * 8) // 包含全部用户信息
+		sessionStorage['userInfo'] = JSON.stringify(payload.userInfo) 
+	},
+	// 设置微信信息
+	SET_WXINFO(state, payload) {
+		state.wxInfo = payload.wxInfo
+	},
+}
+
+// Action 类似于 mutation，不同在于：Action 提交的是 mutation，而不是直接变更状态。Action 可以包含任意异步操作。
+// 使用方法：在组件中使用 this.$store.dispatch('user/xxx') 分发 action，或者使用 mapActions 辅助函数将组件的 methods 映射为 store.dispatch 调用
+const actions = {
+	// 账号、手机号登录
+	login({ commit }, data){
+		return new Promise((rs, rj)=>{
+			// 返回部分用户信息
+			login(data).then(res=>{
+				if (!res){
+					return
+				}
+				commit('DO_LOGIN', { userInfo: res })
+				rs(res)
+			}).catch(err=>{
+				rj(err)
+			})
+		})
+	},
+	// 第三方平台登录
+	thirdLogin({ commit }, data){
+		return new Promise((rs, rj)=>{
+			// 返回部分用户信息
+			thirdpartyLogin(data).then(res=>{
+				if (!res){
+					return
+				}
+				commit('DO_LOGIN', { userInfo: res })
+				rs(res)
+			}).catch(err=>{
+				rj(err)
+			})
+		})
+	},
+	/* logout({commit}){
+		return new Promise((rs,rj)=>{
+			logout().then(res=>{
+				commit('DO_LOGOUT')
+				rs()
+			}).catch(err=>{
+				rj(err)
+			})
+		})
+	}, */
+	// 直接异步函数写法 
+	async logout({ commit }){
+		commit('DO_LOGOUT', await logout())
+	},
+	async getUserInfo({ commit }){
+		// commit('SET_USERINFO', { userInfo: getUserInfo() })
+		return new Promise((rs, rj)=>{
+			// 返回全部用户信息
+			getUserInfo().then(res=>{
+				commit('SET_USERINFO', { userInfo: res })
+				rs(res)
+			}).catch(err=>{
+				rj(err)
+			})
+		})
+	},
+	setUserInfo({ commit }, data){
+		commit('SET_USERINFO', { userInfo: data })
+	}
+}
+
+export default {
+	namespaced: true,
+	state,
+	mutations,
+	actions
+}
