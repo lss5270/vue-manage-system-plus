bd1dc865d7199d5739ba21c433cb017788834377
lihui <81438234@qq.com>
2021-05-08 15:28:43 +0800

    去掉vue3.0废弃语法

--- src/api/index.js
+++ src/api/index.js
@@ -7,5 +7,7 @@ import http from '@/utils/request';
         params: query
     });
 }; */
-export const tableData = data => http.get('/v1/getTableData', data)
+export const getTableData = data => http.post('/v1/getTableData', data)	//获取表格数据
+// get方法mock时，问号后参数导致url地址拦截不正确
+// export const getTableData = data => http.get('/v1/getTableData', {params: data})	
 
