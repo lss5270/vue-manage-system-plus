83fa18586f7c6d6774b34d72a66228e65548396a
lss <81438234@qq.com>
2021-05-08 14:45:53 +0800

    1.mock配置新增 2.表格页面动态获取接口数据、改成async函数 3.开发环境引入mock代理

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
+
+if (process.env.NODE_ENV !== 'production') {	// 开发环境使用mock进行http请求拦截，实际接口联调时注释该代码即可
+	require('./mock')
+}
+
 const app = createApp(App)
 installElementPlus(app)
 app
