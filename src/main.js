06f22283c73be9dbb67f68c20118074fd0972073
lss <81438234@qq.com>
2021-05-11 16:17:47 +0800

    1.恢复哈希模式，兼容打包到github托管 2.打包恢复相对路径，兼容打包到github托管 3.线上访问地址修正 4.恢复打包环境的mock使用，否则托管到github无法登录和请求数据

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
 
-if (process.env.NODE_ENV !== 'production') {	// 开发环境使用mock进行http请求拦截，实际接口联调时注释该代码即可
-	require('./mock')
-}
+// if (process.env.NODE_ENV !== 'production') {	// 开发环境使用mock进行http请求拦截，实际开发时注释该代码块即可
+require('./mock')
+// }
 
 const app = createApp(App)
 installElementPlus(app)
